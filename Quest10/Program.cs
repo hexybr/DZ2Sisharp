b22364147ee25ef56645fb2a254ba5cd30912aef
Handbrake brake <handbrake@mail.ru>
2022-11-07 00:01:56 +0300

    Complete

--- Quest10/Program.cs
+++ Quest10/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число:");
+int num;
+while (!int.TryParse(Console.ReadLine()!, out num))
+{
+    Console.WriteLine("Неверный ввод");
+    Console.WriteLine("Введите число:");
+}
+
+int SecondNumb(int num)
+{
+    int number = num / 10;
+    int number2 = number % 10;
+    return number2;
+}
+Console.WriteLine("Количество десятков в числе:");
+Console.WriteLine(SecondNumb(num));
