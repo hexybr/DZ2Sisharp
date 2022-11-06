6f81b18f64fced44250e379b15d02cd3c51835bd
Handbrake brake <handbrake@mail.ru>
2022-11-07 01:48:49 +0300

    Quest15 complete

--- Quest15/Program.cs
+++ Quest15/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+
+int d = int.Parse(Console.ReadLine()!);
+
+if (d == 1 || d == 2 || d == 3 || d == 4 || d == 5)
+{
+    Console.WriteLine("Будний день");
+}
+else
+if (d == 6 || d == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+    Console.WriteLine("В неделе всего семь дней");
+
