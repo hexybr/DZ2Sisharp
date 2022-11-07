c6aca65a0fe6e94831c4de10d2fbec91f666d740
Handbrake brake <handbrake@mail.ru>
2022-11-07 03:42:17 +0300

    Quest13 complete

--- Quest13/Program.cs
+++ Quest13/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите число:");
+// 1.Ввод числа
+int num;
+if(!int.TryParse(Console.ReadLine()!, out num))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+// 2.Преобразование его в строку
+ string str = Convert.ToString(num);
+ //3.Условие - если в строке больше 2 символов тогда - третья цифра это символ с индексом 2 в строке 
+ //(аналогично массивам индексация в строке начинается с 0) 
+    if (str.Length > 2)
+    {
+        Console.WriteLine("третья цифра - " + str[2]);
+    }
+    // иначе - вывод о том что третьей цифры нет
+    else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
